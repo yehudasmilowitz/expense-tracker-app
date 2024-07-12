7b653777e02c329f40b14192891e5f4c19020fd7
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2024-07-12 00:40:39 -0400

    update import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Header from "./components/Header";
+import Header from "@/app/components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
