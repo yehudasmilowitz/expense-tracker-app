5bdc81dab6f70ad62a56db5e4584eabae9e2a025
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2024-07-12 00:51:47 -0400

    fix import

--- app/components/TransactionList.tsx
+++ app/components/TransactionList.tsx
@@ -1,4 +1,4 @@
-import Transaction from "@/types/transaction";
+import Transaction from "@/types/Transaction";
 import getTransactions from "../actions/getTransactions";
 import TransactionItem from "./TransactionItem";
 
